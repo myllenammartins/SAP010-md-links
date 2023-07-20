a9b05fb97a457a749eef6ee5608aa533d062ed61
unknown <mirandamartins05@gmail.com>
2023-07-20 15:51:51 -0300

    cli funcionando

--- src/index.js
+++ src/index.js
@@ -2,102 +2,36 @@ const fs = require('fs');
 const path = require('path');
 const fetch = require('node-fetch');
 
-// const chalk = require('chalk');
-
-// ler arquivo(file)
-/* function readFile (file) {
-    const isMd = path.extname(file) === '.md';
-    if (!isMd) {
-        const errorMessage = new Error ('Error: O arquivo não é Markdown');
-        return Promise.reject(errorMessage);
-    }
-    return fs.promises.readFile(file).then(data => {
-        return {
-            file: file,
-            data: data.toString()
-        };
-    });
-} */
-
 function readFile(file) {
     const isMd = path.extname(file) === '.md';
     if (!isMd) {
         const errorMessage = new Error('Error: O arquivo não é Markdown');
         return Promise.reject(errorMessage);
     }
-    return fs.promises.readFile(file, 'utf8').then(data => ({
+    return fs.promises.readFile(file, 'utf8').then((data) => ({
         file: file,
-        data: data
+        data: data,
     }));
 }
 
-// ler diretório
-/* function readDir(pathDir) {
-    return new Promise((resolve) => {
-        fs.promises.readdir(pathDir)
-            .then((files) => {
-                const reading = files.filter(file => {
-                    return path.extname(file) === '.md';
-                })
-                    .map(file => {
-                        return readFile(path.resolve(pathDir, file));
-                    });
-                return Promise.all(reading).then((result) => {
-                    resolve(result);
-                });
-            });
-    });
-} */
-
 function readDir(pathDir) {
-    return fs.promises.readdir(pathDir)
-        .then((files) => {
-            const reading = files.filter(file => path.extname(file) === '.md')
-                .map(file => readFile(path.resolve(pathDir, file)));
-            return Promise.all(reading);
-        });
+    return fs.promises.readdir(pathDir).then((files) => {
+        const reading = files
+            .filter((file) => path.extname(file) === '.md')
+            .map((file) => readFile(path.resolve(pathDir, file)));
+        return Promise.all(reading);
+    });
 }
 
-// ler os caminhos (readDir e readFile)
-/* function read(path) {
-    return fs.promises.stat(path)
-        .then(infoStatsObj => {
-            if (infoStatsObj.isDirectory(path)) {
-                return readDir(path);
-            } else {
-                return readFile(path);
-            }
-        });
-} */
-
-/*readDir('./src/file')
-    .then((result) => {
-        console.log(result);
-    }); */
-
-// FUNÇÃO DE STATS
-const statsFunction = (arrayLinks) => {
-
-    return new Promise((resolve)=>{
-        let hrefList = [];
-        let broken = 0;
-        arrayLinks.forEach(element => {
-            hrefList.push(element.href);
-            if(element.ok === false){
-                broken++;
-            }
-        });
-
-        const uniqueLinks = new Set(hrefList);
-
-        const objStats = {
-            total: hrefList.length,
-            unique: uniqueLinks.size,
-            broken: broken,
-        };
-        resolve(objStats);
+function read(path) {
+    return fs.promises.stat(path).then((infoStatsObj) => {
+        if (infoStatsObj.isDirectory(path)) {
+            return readDir(path);
+        } else {
+            return readFile(path);
+        }
     });
-};
+}
 
 function validateLinks(arrayLinks) {
     return Promise.all(
@@ -120,36 +54,55 @@ function validateLinks(arrayLinks) {
 function getLinks(data, filePath) {
     const linkRegex = /\[([^\]]+)\]\(([^)]+)\)/g;
     const matches = [...data.matchAll(linkRegex)];
-    
+
     const links = matches.map((match) => {
         const text = match[1];
         const href = match[2];
         return { href, text, file: filePath };
     });
-  
+
     return links;
 }
 
-function mdLinks(filePath, options = {}) {
-    const absolutePath = path.resolve(filePath);
-  
-    return readFile(absolutePath)
-        .then(({ data }) => {
-            const links = getLinks(data, filePath);
-  
-            if (options.validate) {
-                return validateLinks(links);
-            } else {
-                return { links }; // Retornar um objeto com a propriedade 'links'
-            }
-        });
+function mdLinks(path, options = { validate: false }) {
+    return read(path).then((data) => {
+        const links = getLinks(data.data, data.file);
+        if (!options.validate) {
+            return { links };
+        } else {
+            return validateLinks(links).then((validatedLinks) => {
+                return { links: validatedLinks };
+            });
+        }
+    });
 }
-  
+
+function statsFunction(arrayLinks) {
+    let hrefList = [];
+    let broken = 0;
+    arrayLinks.forEach((element) => {
+        hrefList.push(element.href);
+        if (element.ok === 'fail') {
+            broken++;
+        }
+    });
+
+    const uniqueLinks = new Set(hrefList);
+
+    const objStats = {
+        total: hrefList.length,
+        unique: uniqueLinks.size,
+        broken: broken,
+    };
+    return objStats;
+}
+
 module.exports = {
-    readDir,
-    readFile,
-    //read,
     mdLinks,
-    getLinks,
-    statsFunction
+    statsFunction,
 };
+
+/*read('./src/file/files.md')
+    .then((path) => {
+        console.log(path);
+    });*/
